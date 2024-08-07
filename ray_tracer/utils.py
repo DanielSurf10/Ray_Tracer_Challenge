cff1c40797053e167beb262abea57b781005e46f
Daniel Barbosa Alves <danbarbo@c2r10p4.42sp.org.br>
2024-08-07 19:05:08 -0300

    arrumando testes

---
+++ ray_tracer/utils.py
@@ -0,0 +1,4 @@
+EPSILON = 0.00001
+
+def compare_float(num1, num2):
+	return (abs(num1 - num2) < EPSILON)
--- tests/tests_tuple.py
+++ tests/tests_tuple.py
@@ -1,20 +1,21 @@
 import unittest
 from ray_tracer.tuple import Tuple
+from ray_tracer.utils import compare_float
 
 class TestTuple(unittest.TestCase):
 	def test_tuple_1(self):
 		t = Tuple(4.3, -4.2, 3.1, 1.0)
-		self.assertEqual(t.x, 4.3)
-		self.assertEqual(t.y, -4.2)
-		self.assertEqual(t.z, 3.1)
-		self.assertEqual(t.w, 1.0)
+		self.assertTrue(compare_float(t.x, 4.3))
+		self.assertTrue(compare_float(t.y, -4.2))
+		self.assertTrue(compare_float(t.z, 3.1))
+		self.assertTrue(compare_float(t.w, 1.0))
 
 	def test_tuple_2(self):
 		t = Tuple(4.3, -4.2, 3.1, 0.0)
-		self.assertEqual(t.x, 4.3)
-		self.assertEqual(t.y, -4.2)
-		self.assertEqual(t.z, 3.1)
-		self.assertEqual(t.w, 1.0)
+		self.assertTrue(compare_float(t.x, 4.3))
+		self.assertTrue(compare_float(t.y, -4.2))
+		self.assertTrue(compare_float(t.z, 3.1))
+		self.assertTrue(compare_float(t.w, 0.0))
 
 if __name__ == '__main__':
 	unittest.main()
